7420947b1c6d17b40bd11ef52551c754d31de509
nurselaine <elaine.t.huynh@gmail.com>
2022-09-23 14:08:09 -0700

    all routes working and tests passing. Added a 'pots' table to database

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 'use strict';
 
 const { start } = require('./src/server');
-const { sequelizeDatabase, PlantsModel } = require('./src/models/index');
+const { sequelizeDatabase, PlantsModel, PotsModel } = require('./src/models/index');
 
 sequelizeDatabase.sync()
   .then(() => {
     console.log('successfully connected to DB!');
     // PlantsModel.create({ name: 'Daisy' });
+    // PotsModel.create({name: 'elaine', size: 4});
     start();
   })
   .catch(error => console.error('ERROR: ', error.message));
 
 // start();
+
+
--- src/models/index.js
+++ src/models/index.js
@@ -1,9 +1,11 @@
 'use strict';
 
-const { Sequelize, DataTypes } = require('sequelize');
-const plantsSchema = require('./plant.schema');
-const PlantInterface = require('./plantInterface');
 require('dotenv').config();
+const { Sequelize, DataTypes } = require('sequelize');
+
+const plantsSchema = require('./plant.schema');
+const potSchema = require('./pot.schema');
+const ModelInterface = require('./ModelInterface');
 
 const DATABASE_URL = process.env.NODE_ENV === 'test'
   ? 'sqlite:memory'
@@ -13,6 +15,7 @@ const DATABASE_URL = process.env.NODE_ENV === 'test'
 const sequelizeDatabase = new Sequelize(DATABASE_URL);
 
 const PlantsModel = plantsSchema(sequelizeDatabase, DataTypes);
+const PotsModel = potSchema(sequelizeDatabase, DataTypes);
 
 // ********* JOINS Tables **************
 
@@ -20,5 +23,7 @@ module.exports = {
   sequelizeDatabase,
   DataTypes,
   PlantsModel,
-  plantsInterface: new PlantInterface(PlantsModel),
+  plantsInterface: new ModelInterface(PlantsModel),
+  PotsModel,
+  potsInterface: new ModelInterface(PotsModel),
 };
